ca0d9ae9c46008550b31f0242876a674bc9c2471
cchamchi <cchamchi@naver.com>
2019-06-10 18:20:39 +0900

    add  pin map

--- rhea_rover.ino
+++ rhea_rover.ino
@@ -6,10 +6,14 @@
 #include "src/rover_Lx16A.h"
 #include "src/rover_motor.h"
 
-/* software serial pin map */
-// GPS : 10,11
-// BLE : 4,5
+/* Arduino UNO pin map */
+// GPS : 8,9
+// BLE : 4,5 (fix)
 // Lx16A : 6,7
+// SD :  MOSI 11, MISO 12, CLK 13, CS 10
+// SHT : SCL,SDA
+// MQ7 : A0
+// RF & Serial print : 1,2
 
 int joy_velocity=0,joy_raduis=0;
 
