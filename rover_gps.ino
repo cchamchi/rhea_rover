12f07acd1b94a64f90354b1b301825075d8c2723
cchamchi <cchamchi@naver.com>
2019-06-11 17:46:26 +0900

    modify setup

--- rover_gps.ino
+++ rover_gps.ino
@@ -1,4 +1,4 @@
-SoftwareSerial _GPSport(10,11);
+SoftwareSerial _GPSport(9,10);
 
 RoverGPS<SoftwareSerial> roverGPS(_GPSport);
 
@@ -6,30 +6,31 @@ void setupRoverGps(){
 
   // Causion!! When using a Software serial, 
   // must use 9600 baud rate, cause a Software read speed is too late
+  Serial.println(F("Start GPS at SoftwareSerial 9,10 "));
   roverGPS.begin(9600);
 }
 
 void loopRoverGps(){
 
-  if(roverGPS.read()){  
-       Serial.print("date=");
-       Serial.println(roverGPS.date());
-       Serial.print("time=");
-       Serial.println(roverGPS.time()/100.);
-       Serial.print("lat=");
-       Serial.println((roverGPS.location().lat* 1.0e-7f),7);
-       Serial.print("lon=");
-       Serial.println((roverGPS.location().lng* 1.0e-7f),7);       
-       Serial.print("altitude(m)=");
-       Serial.println(roverGPS.location().alt/100.); 
-       Serial.print("num_sat=");
-       Serial.println(roverGPS.num_sats()); 
-       Serial.print("speed(km/s)=");
-       Serial.println(roverGPS.ground_speed_km());
-       Serial.print("cource=");
-       Serial.println(roverGPS.ground_course());
-       Serial.println("*********************************"); 
-  }   
+  // if(roverGPS.read()){  
+  //      Serial.print("date=");
+  //      Serial.println(roverGPS.date());
+  //      Serial.print("time=");
+  //      Serial.println(roverGPS.time()/100.);
+  //      Serial.print("lat=");
+  //      Serial.println((roverGPS.location().lat* 1.0e-7f),7);
+  //      Serial.print("lon=");
+  //      Serial.println((roverGPS.location().lng* 1.0e-7f),7);       
+  //      Serial.print("altitude(m)=");
+  //      Serial.println(roverGPS.location().alt/100.); 
+  //      Serial.print("num_sat=");
+  //      Serial.println(roverGPS.num_sats()); 
+  //      Serial.print("speed(km/s)=");
+  //      Serial.println(roverGPS.ground_speed_km());
+  //      Serial.print("cource=");
+  //      Serial.println(roverGPS.ground_course());
+  //      Serial.println("*********************************"); 
+  // }   
        
 }
 
