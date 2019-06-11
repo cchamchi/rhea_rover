dfcad4797625d6ba64799271c30f536126240f1a
cchamchi <cchamchi@naver.com>
2019-06-11 17:45:42 +0900

    modify pm2.5

--- rover_PM2_5.ino
+++ rover_PM2_5.ino
@@ -1,35 +1,39 @@
-rover_PM2_5 mypm(A0);
+RoverPM2_5 roverPM2_5(A1,3);
 float dustDensity;
 
-void loopRoverPM2_5(){
-  mypm.calulatePM2_5(dustDensity);
+void setupRoverPM2_5(){
+  roverPM2_5.begin();
+}
+
+void updateRoverPM2_5(){
+  dustDensity=roverPM2_5.calulatePM2_5();
   
-  if(dustDensity<0){
-    Serial.println("Waiting...");
-  }
-  if(dustDensity<30){
-    Serial.println("What a NICE Air!");
-    Serial.print("Density: ");
-    Serial.print(dustDensity);
-    Serial.println("μm/m^3");
-  }
-  else if(dustDensity<80){
-    Serial.println("It's GOOD.");
-    Serial.print("Density: ");
-    Serial.print(dustDensity);
-    Serial.println("μm/m^3");
-  }
-  else if(dustDensity<150){
-    Serial.println("It's pretty BAD. Please keep your child from going out.");
-    Serial.print("Density: ");
-    Serial.print(dustDensity);
-    Serial.println("μm/m^3");
-  }
-  else{
-    Serial.println("DO NEVER GO OUT!");
-    Serial.print("Density: ");
-    Serial.print(dustDensity);
-    Serial.println("μm/m^3");
-  }  
-  delay(1000);
+  // if(dustDensity<0){
+  //   Serial.println("Waiting...");
+  // }
+  // if(dustDensity<30){
+  //   Serial.println("What a NICE Air!");
+  //   Serial.print("Density: ");
+  //   Serial.print(dustDensity);
+  //   Serial.println("μm/m^3");
+  // }
+  // else if(dustDensity<80){
+  //   Serial.println("It's GOOD.");
+  //   Serial.print("Density: ");
+  //   Serial.print(dustDensity);
+  //   Serial.println("μm/m^3");
+  // }
+  // else if(dustDensity<150){
+  //   Serial.println("It's pretty BAD. Please keep your child from going out.");
+  //   Serial.print("Density: ");
+  //   Serial.print(dustDensity);
+  //   Serial.println("μm/m^3");
+  // }
+  // else{
+  //   Serial.println("DO NEVER GO OUT!");
+  //   Serial.print("Density: ");
+  //   Serial.print(dustDensity);
+  //   Serial.println("μm/m^3");
+  // }  
+  // delay(1000);
 }
