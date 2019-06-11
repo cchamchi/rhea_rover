3d7dda6edd7ae76ac85b3cca3f29a465f32cd399
cchamchi <cchamchi@naver.com>
2019-06-11 17:45:27 +0900

    modify pm2.5

--- src/rover_PM2_5.cpp
+++ src/rover_PM2_5.cpp
@@ -11,27 +11,39 @@
 #include "Arduino.h"
 
 
-rover_PM2_5::rover_PM2_5(int Vo, int V_LED){
+RoverPM2_5::RoverPM2_5(int Vo, int V_LED){
   _Vo=Vo;
   _V_LED=V_LED;
-  
+  _dustDensity = 0;
+  _last_millis=0;
+}
+void RoverPM2_5::begin(){
   pinMode(_V_LED,OUTPUT);
-  pinMode(_Vo,INPUT);
 }
-rover_PM2_5::rover_PM2_5(int Vo){
-  _Vo=Vo;
-  pinMode(_Vo,INPUT);
-}
+float RoverPM2_5::calulatePM2_5(){
+  float Vo_value,voltage;
+  long current_millis=millis();
+  if((_last_millis-current_millis) > SLEEP_TIME ){
+    _last_millis=current_millis;
+    digitalWrite(_V_LED,LOW);  //power on LED
+    delayMicroseconds(SAMPLING_TIME);
 
-void rover_PM2_5::calulatePM2_5(float _dustDensity){
-  digitalWrite(_V_LED,LOW);
-  delayMicroseconds(280);
-  Vo_value =analogRead(_Vo);
-  delayMicroseconds(40);
-  digitalWrite(_V_LED,HIGH);
-  delayMicroseconds(9680);
-  voltage = Vo_value*5.0/1024.0;
-  _dustDensity = (voltage-0.3)/0.005;
+    Vo_value =analogRead(_Vo); //read a dust Vo_value
 
-  return _dustDensity;
+    delayMicroseconds(DELTA_TIME);
+    digitalWrite(_V_LED,HIGH); //turn off LED
+    //delayMicroseconds(SLEEP_TIME);
+
+    voltage = Vo_value*(5.0/1024.0);  // 0 - 5V mapped to 0 - 1023 integer values
+    // linear eqaution taken from http://www.howmuchsnow.com/arduino/airquality/
+    // Chris Nafis (c) 2012
+    //_dustDensity = (voltage-0.3)/0.005;// where are you from?
+    _dustDensity = 0.17 * voltage - 0.1;
+
+    return _dustDensity;
+  }else{
+    Serial.println(F("update need at least 1second "));
+    _last_millis=current_millis;
+    return(-1);
+  }
 }
--- src/rover_PM2_5.h
+++ src/rover_PM2_5.h
@@ -1,24 +1,38 @@
+/**
+* @file 
+* @author SeungMin Shin, Haneulbit Kim, Chan Lee
+* @license This project is released under the MIT License (MIT)
+* @copyright Copyright (c) 2019 Asgardia
+* @date Mar 2019
+* @brief ...
+*/
+
+
+
+/*  Sharp Optical Dust Sensor GP2Y1010AU0F
+
+출처: https://bbangpan.tistory.com/51 [빵판닷컴 (bbangpan.com)]
+
+*/
+
 #ifndef rover_PM2_5_H
 #define rover_PM2_5_H
 
-class rover_PM2_5{
-  public:
-    rover_PM2_5(int Vo, int V_LED);
-    rover_PM2_5(int Vo);
-    void calulatePM2_5(float _dustDensity);
-    float Vo_value = 0;
-    float voltage = 0;
-    float _dustDensity = 0;
 
-  
-  
+#define SAMPLING_TIME 280
+#define DELTA_TIME 40
+#define SLEEP_TIME 9680
+class RoverPM2_5{
+  public:
+    RoverPM2_5(int Vo, int V_LED);
+    void begin();
+    float calulatePM2_5();
   
   private:
-
-
-
+    float _dustDensity ;
     int _Vo;
     int _V_LED;
+    long _last_millis;
 
 
 };
